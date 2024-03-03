69eb2802057f220d72cf3cd580da452b5b18931a
John O'Reilly <oreillyj@gmail.com>
2024-03-03 15:27:24 +0000

    dependency updates

--- compose-desktop/build.gradle.kts
+++ compose-desktop/build.gradle.kts
@@ -1,6 +1,3 @@
-import org.jetbrains.compose.compose
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 plugins {
     kotlin("jvm")
     id("org.jetbrains.compose") version Versions.composeDesktopWeb
@@ -19,10 +16,6 @@ dependencies {
     implementation(compose.desktop.currentOs)
     implementation(project(":shared"))
 }
-
-//tasks.withType<KotlinCompile> {
-//    kotlinOptions.jvmTarget = "1.8"
-//}
 
 application {
     mainClass.set("MainKt")
--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -6,61 +6,37 @@ plugins {
 }
 
 kotlin {
-    android()
+    androidTarget()
     jvm()
 
-    val iosArm64 = iosArm64()
-    val iosX64 = iosX64()
-    val iosSimulatorArm64 = iosSimulatorArm64()
+    listOf(
+        iosX64(),
+        iosArm64(),
+        iosSimulatorArm64()
+    ).forEach {
+        it.binaries.framework {
+            baseName = "shared"
+        }
+    }
 
     sourceSets {
-        val commonMain by getting {
-            dependencies {
-                api(Kotlinx.coroutinesCore)
-                implementation(Square.okio)
-            }
+        commonMain.dependencies {
+            api(Kotlinx.coroutinesCore)
+            implementation(Square.okio)
         }
 
-        val commonTest by getting {
-            dependencies {
-                implementation(Kotlinx.coroutinesTest)
-                implementation(kotlin("test-common"))
-                implementation(kotlin("test-annotations-common"))
-            }
-        }
-        val androidMain by getting {
-            dependencies {
-            }
-        }
+//        commonTest.dependencies {
+//            implementation(Kotlinx.coroutinesTest)
+//            implementation(kotlin("test-common"))
+//            implementation(kotlin("test-annotations-common"))
+//        }
+
         val androidUnitTest by getting {
             dependencies {
                 implementation(kotlin("test-junit"))
                 implementation("junit:junit:4.13.2")
             }
         }
-        val appleMain by creating {
-            dependsOn(commonMain)
-        }
-        val appleTest by creating {
-            dependsOn(commonTest)
-        }
-
-        val jvmMain by getting
-
-        listOf(
-            iosArm64, iosX64, iosSimulatorArm64
-        ).forEach {
-            it.binaries.framework {
-                baseName = "shared"
-            }
-            getByName("${it.targetName}Main") {
-                dependsOn(appleMain)
-            }
-            getByName("${it.targetName}Test") {
-                dependsOn(appleTest)
-            }
-        }
-
     }
 }
 
