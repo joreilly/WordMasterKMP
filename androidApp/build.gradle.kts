c87262a6e715b768bed07ad9779cb083409a5c7f
John O'Reilly <oreillyj@gmail.com>
2025-07-20 13:41:10 +0100

    dependency updates + android style updates

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -45,10 +45,8 @@ dependencies {
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.compose.foundation)
     implementation(libs.androidx.compose.foundation.layout)
-    implementation(libs.androidx.compose.material)
     implementation(libs.androidx.compose.runtime)
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.tooling)
     implementation(libs.androidx.compose.material3)
-    implementation(libs.accompanist.insets)
 }
